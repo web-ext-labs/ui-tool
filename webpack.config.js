dbaac58ad1f72bf48a31d70c88450daa28f2cf58
Bun <hello@bunfires.com>
2022-07-10 11:30:04 +0100

    Migrate to Webpack 5 (#59)

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin')
+const path = require('path')
 
 module.exports = {
   entry: './src/script.js',
   output: {
     path: path.join(__dirname, 'public'),
-    filename: 'build.js',
+    filename: 'build.js'
   },
-  plugins: [
-    new UglifyJsPlugin()
-  ]
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()]
+  }
 }
